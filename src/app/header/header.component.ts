b0ce203bf374fa95d2d66a7b58faceaa818bacd7
TheTzunder30 <leocedsan@campus.monlau.com>
2024-03-12 15:35:03 +0100

    a

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,33 @@ import { BarraComponent } from '../barra/barra.component';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  
-  
+ 
+    changeBack() {
+      let backSwap = document.getElementById('listB');
+      if (backSwap) {
+        backSwap.style.backgroundColor = 'green';
+      }
+    }
+   
+    changeBack2() {
+      let backSwap = document.getElementById('listB');
+      if (backSwap) {
+        backSwap.style.backgroundColor = 'red';
+      }
+    }
+   
+    changeBack3() {
+      let backSwap = document.getElementById('listB');
+      if (backSwap) {
+        backSwap.style.backgroundColor = 'blue';
+      }
+    }
+    changeBack4() {
+      let backSwap = document.getElementById('listB');
+      if (backSwap) {
+        backSwap.style.backgroundColor = '#6b6565';
+      }
+    }
+  }
 
-}
+
